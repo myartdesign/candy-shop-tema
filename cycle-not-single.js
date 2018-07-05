87ed3fd1486ba1643884730800d5a75885a3214f
myartdesign <40593038+myartdesign@users.noreply.github.com>
2018-07-05 18:26:59 -0300

    Add files via upload

---
+++ cycle-not-single.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function() {
+		jQuery('#s1').cycle({
+			fx: 'scrollDown',
+			speed:  600,
+		});
+	});
+	
+	jQuery(document).ready(function() {
+		jQuery('#s2').cycle({
+			fx: 'scrollUp',
+			speed:  600,
+		});
+	});
+	
+	jQuery(document).ready(function() {
+		jQuery('#s3').cycle({
+			fx: 'scrollDown',
+			speed:  600,
+		});
+	});
